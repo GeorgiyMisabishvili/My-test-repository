1bcb8fd449870f3b5958d2590432a10b0cf332cc
Georgiy <108755494+GeorgiyMisabishvili@users.noreply.github.com>
2022-08-15 13:24:10 +0300

    first commit

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className="content">
+    <div >
+      <img src="https://cdn1.vectorstock.com/i/1000x1000/05/00/gaming-night-stream-banner-with-teen-gamer-room-vector-37120500.jpg" className="profile-header_img"></img>
+    </div>
+    <div>
+      ava+descriptions
+    </div>
+    <div>
+      My post
+      <div>
+        New Post
+      </div>
+      <div>
+        <div>
+          Post 1
+        </div>
+         <div>
+          Post 3
+        </div>
+      </div>
+    </div>
+  </div>
+
+}
+
+export default Profile; 
